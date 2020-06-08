2483fd62a8f1d441dd6a82875fce7420fe05b688
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-06-08 06:42:56 +0530

    Updates

---
+++ precache-manifest.289d377ae9933a3a082f3cb2706f4669.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "989017016a32acf714c9baa0a6aff3c1",
+    "url": "/frontend-team/index.html"
+  },
+  {
+    "revision": "dbc440afa26b62c0012e",
+    "url": "/frontend-team/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0ab7ea8240a41731e2d9",
+    "url": "/frontend-team/static/css/main.c37223da.chunk.css"
+  },
+  {
+    "revision": "dbc440afa26b62c0012e",
+    "url": "/frontend-team/static/js/2.78982ec8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/frontend-team/static/js/2.78982ec8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ab7ea8240a41731e2d9",
+    "url": "/frontend-team/static/js/main.0cc3fcf4.chunk.js"
+  },
+  {
+    "revision": "00fc76084f1cf1d3e331",
+    "url": "/frontend-team/static/js/runtime-main.9c770acd.js"
+  },
+  {
+    "revision": "fed70ed7224387996fb0ae93c846da99",
+    "url": "/frontend-team/static/media/navbarBurgerMenuIcon.fed70ed7.png"
+  }
+]);
