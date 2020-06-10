cf9330092cc45344b0182ee7c50c201588283c1d
Satyam52 <satyam0350@gmail.com>
2020-06-10 20:39:44 +0530

    added Modal in UpcomingActivity Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
-import './App.css';
-import { Navigation } from './Components/Navigation/Navigation';
-import HomePage from './Components/HomePage/HomePage';
+import React from "react";
+import "./App.css";
+import "swiper/css/swiper.css";
+import { Navigation } from "./Components/Navigation/Navigation";
+import HomePage from "./Components/HomePage/HomePage";
 
 function App() {
   return (
