c27d6a31d6f614e545b7fdbe7c761d6cac4e6aa9
Satyam52 <satyam0350@gmail.com>
2020-06-11 08:01:25 +0530

    addes missing swiper css in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import './App.css';
-import { Navigation } from './Components/Navigation/Navigation';
-import HomePage from './Components/HomePage/HomePage';
-import Footer from './Components/Footer/Footer';
-
+import React from "react";
+import "./App.css";
+import { Navigation } from "./Components/Navigation/Navigation";
+import HomePage from "./Components/HomePage/HomePage";
+import Footer from "./Components/Footer/Footer";
+import "swiper/css/swiper.css";
 function App() {
   return (
     <div className="App">
