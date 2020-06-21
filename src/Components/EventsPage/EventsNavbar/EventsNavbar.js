faba0908b77c1b12cd2023586b1d574312cc2c77
AMITKESARI2000 <kesari.amit33@gmail.com>
2020-06-21 19:14:50 +0530

    added styling and transition to events navbar

--- src/Components/EventsPage/EventsNavbar/EventsNavbar.js
+++ src/Components/EventsPage/EventsNavbar/EventsNavbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Navbar, Nav } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./EventsNavbar.css";
 
@@ -7,7 +7,7 @@ class EventsNavbar extends Component {
   render() {
     return (
       <div className="nav-container-EventsNavbar">
-        <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
+        <Navbar collapseOnSelect expand="lg" variant="dark" sticky="bottom">
           <Navbar.Brand href="#home" className="main-web-name-EventsNavbar">
             Tech Hub IIT Tirupati
           </Navbar.Brand>
@@ -17,18 +17,23 @@ class EventsNavbar extends Component {
               <Nav.Link href="#all-events" className="nav-link-EventsNavbar ">
                 ALL EVENTS
               </Nav.Link>
+
               <Nav.Link href="#workshops" className="nav-link-EventsNavbar ">
                 WORKSHOPS
               </Nav.Link>
+
               <Nav.Link href="#talks" className="nav-link-EventsNavbar ">
                 TALKS
               </Nav.Link>
+
               <Nav.Link href="#activities" className="nav-link-EventsNavbar ">
                 ACTIVITIES
               </Nav.Link>
+
               <Nav.Link href="#competitions" className="nav-link-EventsNavbar ">
                 COMPETITIONS
               </Nav.Link>
+
               <Nav.Link
                 href="#calender-events"
                 className="nav-link-EventsNavbar "
