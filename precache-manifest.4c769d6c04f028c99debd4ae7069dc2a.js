32cf2c2595d8ffd4bfec4367751b284e89a5cfee
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-06-27 10:13:43 +0530

    Updates

---
+++ precache-manifest.4c769d6c04f028c99debd4ae7069dc2a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a1434eaad92411813bd0d90bc86397",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "bf3c4a689aa55a08bba2",
+    "url": "/home/static/css/2.8ae9822a.chunk.css"
+  },
+  {
+    "revision": "6e50ea68a0e6e908611a",
+    "url": "/home/static/css/main.fd4f7c8c.chunk.css"
+  },
+  {
+    "revision": "bf3c4a689aa55a08bba2",
+    "url": "/home/static/js/2.8542a45a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/home/static/js/2.8542a45a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e50ea68a0e6e908611a",
+    "url": "/home/static/js/main.57d5f879.chunk.js"
+  },
+  {
+    "revision": "48fb55b5aac4e04c963b",
+    "url": "/home/static/js/runtime-main.0d5627fd.js"
+  },
+  {
+    "revision": "9d76ef0b6e446a7c775808450beb1e4b",
+    "url": "/home/static/media/GetInspiredBulbIcon.9d76ef0b.png"
+  },
+  {
+    "revision": "3f6febd19a3750664c77cb31fc62b1ec",
+    "url": "/home/static/media/eventsPageLogo.3f6febd1.png"
+  },
+  {
+    "revision": "fed70ed7224387996fb0ae93c846da99",
+    "url": "/home/static/media/navbarBurgerMenuIcon.fed70ed7.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/frontend-team/precache-manifest.250c6c536fcae1304c2b86586a8838aa.js"
+  "/home/precache-manifest.4c769d6c04f028c99debd4ae7069dc2a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/frontend-team/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
