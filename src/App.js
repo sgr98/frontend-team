076ccf5aaa3df4c3ed855d6e727cca378c289296
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-06-30 00:46:39 +0530

    Added BlogsPage to app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import HomePage from './Components/HomePage/HomePage';
 import Footer from './Components/Footer/Footer';
 import 'swiper/css/swiper.css';
 import EventsPage from './Components/EventsPage/EventsPage';
+import BlogsPage from './Components/BlogsPage/BlogsPage';
 
 function App() {
   return (
@@ -14,7 +15,8 @@ function App() {
         {/* <Navigation /> */}
 
         <Switch>
-          <Route path={['/', '/home']} exact component={HomePage} />
+          {/* <Route path={['/', '/home']} exact component={HomePage} /> */}
+          <Route path={['/', '/home']} exact component={BlogsPage} />
           <Route path="/Events" component={EventsPage} />
 
           <Route component={Error} />
