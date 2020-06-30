421075ff961c5be9c4474c699d0d8918f4dbfac4
sgr98 <60283755+sgr98@users.noreply.github.com>
2020-06-30 19:47:48 +0530

    Adding BlogsNavbar.js and BlogsNavbar.css

--- src/Components/BlogsPage/BlogsPage.js
+++ src/Components/BlogsPage/BlogsPage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import SearchBar from './SearchBar/SearchBar';
+import BlogsNavbar from './BlogsNavbar/BlogsNavbar';
 
 const BlogsPage = () => {
   return (
     <div>
+      <BlogsNavbar />
       <SearchBar />
       BlogsPage
     </div>
