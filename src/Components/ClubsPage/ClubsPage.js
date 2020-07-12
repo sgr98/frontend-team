691333af5baa3919a8906993739a991eeed52d0d
sgr98 <60283755+sgr98@users.noreply.github.com>
2020-07-12 18:37:50 +0530

    Adding ClubsNavbar

--- src/Components/ClubsPage/ClubsPage.js
+++ src/Components/ClubsPage/ClubsPage.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ClubsTeamInfo } from './ClubsTeamInfo/ClubsTeamInfo';
 import ContactForm from './ContactForm/ContactForm';
 import CustomHR from '../ReusableComponents/CustomHR/CustomHR';
+import ClubsNavbar from './ClubsNavbar/ClubsNavbar';
 
 const ClubsPage = () => {
   return (
