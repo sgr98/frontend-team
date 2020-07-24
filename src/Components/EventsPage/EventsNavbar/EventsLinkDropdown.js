f5d3b65e8d66ad66c3018122b4eaf398ce2259ad
sgr98 <60283755+sgr98@users.noreply.github.com>
2020-07-24 23:40:00 +0530

    Added mobile ver (EventNavDropdown)

---
+++ src/Components/EventsPage/EventsNavbar/EventsLinkDropdown.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { NavDropdown } from 'react-bootstrap';
+import './EventsLinkDropdown.css';
+import DropdownIcon from '../../BlogsPage/SearchBar/DropdownIcon.png';
+
+export class EventsLinkDropdown extends Component {
+
+    render() {
+        return (
+            <div className="main-container-EventsDropContainer">
+                <NavDropdown
+                    className="nav-dropdown-EventsDropDown"
+                    href="#all-events"
+                    title={
+                        <div><span>
+                        All Events
+                        <img
+                            src={DropdownIcon}
+                            alt="DropdownIcon"
+                            style={{ height: '0.5em', margin: '0 0 3px 9px' }}
+                        />
+                        </span></div>
+                    }
+                    id="collasible-nav-dropdown-EventsDropDown"
+                    >
+                    <NavDropdown.Item
+                        href="#AllEvents"
+                        className="menuitem-EventsDropDown"
+                    >
+                        All Events
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                        href="#Workshops"
+                        className="menuitem-EventsDropDown"
+                    >
+                        Workshops
+                    </NavDropdown.Item>
+
+                    <NavDropdown.Item
+                        href="#Talks"
+                        className="menuitem-EventsDropDown"
+                    >
+                        talks
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                        href="#Activities"
+                        className="menuitem-EventsDropDown"
+                    >
+                        Activities
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                        href="#Competitions"
+                        className="menuitem-EventsDropDown"
+                    >
+                        Competitions
+                    </NavDropdown.Item>
+                    <NavDropdown.Item
+                        href="#CalendarEvents"
+                        className="menuitem-EventsDropDown"
+                    >
+                        Calendar Events
+                    </NavDropdown.Item>
+                </NavDropdown>
+            </div>
+        );
+    }
+
+}
+
+export default EventsLinkDropdown;
--- src/Components/EventsPage/EventsNavbar/EventsNavbar.js
+++ src/Components/EventsPage/EventsNavbar/EventsNavbar.js
@@ -3,6 +3,8 @@ import { Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./EventsNavbar.css";
+import "./EventsLinkDropdown";
+import EventsLinkDropdown from "./EventsLinkDropdown";
 
 export class EventsNavbar extends Component {
   constructor(props) {
@@ -45,83 +47,88 @@ export class EventsNavbar extends Component {
     }
 
     return (
-      <div className={mainClassName}>
-        <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
-          <Navbar.Brand className="main-web-name-EventsNavbar" href="/events">
-            Events
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="w-100 justify-content-around">
-              {/* ALL EVENTS */}
+      <div className="switcher-class-EventsNavbar">
+        <div className={mainClassName}>
+          <Navbar collapseOnSelect expand="lg" variant="dark" sticky="top">
+            <Navbar.Brand className="main-web-name-EventsNavbar" href="/events">
+              Events
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="w-100 justify-content-around">
+                {/* ALL EVENTS */}
 
-              <div className={navContainerClass}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/AllEvents`}
-                  id="events-navbar-AllEvents"
-                >
-                  <div>ALL EVENTS</div>
-                </NavLink>
-              </div>
+                <div className={navContainerClass}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/AllEvents`}
+                    id="events-navbar-AllEvents"
+                  >
+                    <div>ALL EVENTS</div>
+                  </NavLink>
+                </div>
 
-              {/* WORKSHOPS */}
-              <div className={navContainerClass}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/Workshops`}
-                  id="events-navbar-Workshops"
-                >
-                  <div>WORKSHOPS</div>
-                </NavLink>
-              </div>
+                {/* WORKSHOPS */}
+                <div className={navContainerClass}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/Workshops`}
+                    id="events-navbar-Workshops"
+                  >
+                    <div>WORKSHOPS</div>
+                  </NavLink>
+                </div>
 
-              {/* TALKS */}
-              <div className={navContainerClass}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/Talks`}
-                  id="events-navbar-Talks"
-                >
-                  <div>TALKS</div>
-                </NavLink>
-              </div>
+                {/* TALKS */}
+                <div className={navContainerClass}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/Talks`}
+                    id="events-navbar-Talks"
+                  >
+                    <div>TALKS</div>
+                  </NavLink>
+                </div>
 
-              {/* ACTIVITIES */}
-              <div className={navContainerClass}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/Activities`}
-                  id="events-navbar-Activities"
-                >
-                  <div>ACTIVITIES</div>
-                </NavLink>
-              </div>
+                {/* ACTIVITIES */}
+                <div className={navContainerClass}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/Activities`}
+                    id="events-navbar-Activities"
+                  >
+                    <div>ACTIVITIES</div>
+                  </NavLink>
+                </div>
 
-              {/* COMPETITIONS */}
-              <div className={navContainerClass}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/Competitions`}
-                  id="events-navbar-Competitions"
-                >
-                  <div>COMPETITIONS</div>
-                </NavLink>
-              </div>
+                {/* COMPETITIONS */}
+                <div className={navContainerClass}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/Competitions`}
+                    id="events-navbar-Competitions"
+                  >
+                    <div>COMPETITIONS</div>
+                  </NavLink>
+                </div>
 
-              {/* CALENDAR EVENTS */}
-              <div className={`calendarLink-EventsNavbar ${navContainerClass}`}>
-                <NavLink
-                  className={navLinkClass}
-                  to={`${url}/CalendarEvents`}
-                  id="events-navbar-CalendarEvents"
-                >
-                  <div>CALENDAR EVENTS</div>
-                </NavLink>
-              </div>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+                {/* CALENDAR EVENTS */}
+                <div className={`calendarLink-EventsNavbar ${navContainerClass}`}>
+                  <NavLink
+                    className={navLinkClass}
+                    to={`${url}/CalendarEvents`}
+                    id="events-navbar-CalendarEvents"
+                  >
+                    <div>CALENDAR EVENTS</div>
+                  </NavLink>
+                </div>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+        </div>
+
+        <EventsLinkDropdown />
+
       </div>
     );
   }
