30e97bd2b5f0900a6bbfd6661547e8a95d06945a
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-07-25 23:33:10 +0530

    Styling changes
    new design format

--- src/Components/AchievementsPage/AchievementCard/AchievementCard.js
+++ src/Components/AchievementsPage/AchievementCard/AchievementCard.js
@@ -1,21 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react'
 import './AchievementCard.css';
 import ReadMoreReact from 'read-more-react';
-import Moment from 'react-moment';
-import { Button } from 'react-bootstrap';
 
-const Card = ({ 
+const Card_template = ({ 
     single
 }) => {
     return (
       <div className="achievement-card-container">
         <div className="achievement-card-content">
+
           <div className="achievement-card-text">
-            <p className="achievement-card-date">
-            <Moment format="DD MMMM YYYY">
-            {single.date}
-            </Moment>
-            </p>
             <p className="achievement-card-title">{single.title}</p>
             <p className="achievement-card-caption">{single.caption}</p>
             <p className="achievement-card-description">
@@ -27,25 +21,19 @@ const Card = ({
               readMoreText=".. Read More ..."
             />
             </p>
-            <Button variant="outline" className="card-btn-AchievementCard">
-                VIEW PROJECT
-            </Button>
           </div>
-          <div className="achievement-card-content-image-holder">
-          <div
-            className="achievement-card-content-image"
-            style={{backgroundImage:
+          <img className="achievement-card-image"
+            src={
               single.image
                 ? single.image
-                : 'url(https://images.unsplash.com/photo-1543722615-dbf5edf1a00d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80)'
-            }}
+                : 'https://images.unsplash.com/photo-1543722615-dbf5edf1a00d?ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80'
+            }
           /> 
-          </div> 
-        </div>
 
-      </div>
+        </div>
+          </div> 
     );
   };
   
-  export default Card;
+  export default Card_template;
   
