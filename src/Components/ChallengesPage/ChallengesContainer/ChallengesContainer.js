7cc3eb79610dd50ee06cbc22ac7a5b509db2fbd8
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-07-26 01:22:33 +0530

    Challenges Page Dropdown

--- src/Components/ChallengesPage/ChallengesContainer/ChallengesContainer.js
+++ src/Components/ChallengesPage/ChallengesContainer/ChallengesContainer.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ChallengesContainer.css';
 import ChallengeCard from './ChallengeCard/ChallengeCard';
+import ChallengesDropdown from './ChallengesDropdown/ChallengesDropdown';
 
 const ChallengesContainer = () => {
   return (
     <div className="root-ChallengesContainer">
+      <ChallengesDropdown />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
+      <ChallengeCard />
       <ChallengeCard />
       <ChallengeCard />
       <ChallengeCard />
