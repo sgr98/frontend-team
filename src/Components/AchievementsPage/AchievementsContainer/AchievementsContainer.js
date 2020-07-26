901207f6c0d3144266f16b315bc61b87689144ab
Mir Sameed Ali <mirsameedali@Mirs-MacBook-Pro.local>
2020-07-26 18:03:37 +0530

    Styled Modal in achievements Page

--- src/Components/AchievementsPage/AchievementsContainer/AchievementsContainer.js
+++ src/Components/AchievementsPage/AchievementsContainer/AchievementsContainer.js
@@ -1,62 +1,76 @@
 import React, { useState } from 'react';
 import './AchievementsContainer.css';
 import Card from '../AchievementCard/AchievementCard';
-import { Modal,Carousel } from 'react-bootstrap';
+import { Modal, Carousel } from 'react-bootstrap';
 
-const AchievementsContainer = ({
-  achievementsData=[],year=""
-}) => {
+const AchievementsContainer = ({ achievementsData = [], year = '' }) => {
+  const [show, setShow] = useState(false);
+  const [gallery, setGallery] = useState([]);
+  const handleClose = () => setShow(false);
+  const handleShow = (e, gallery_a) => {
+    if (e.target.className != 'read-more-button') {
+      setGallery(gallery_a);
+      setShow(true);
+    }
+  };
 
-    const [show, setShow] = useState(false);
-    const [gallery, setGallery] = useState([]);
-    const handleClose = () => setShow(false);
-    const handleShow = (e,gallery_a) => {if(e.target.className!="read-more-button"){setGallery(gallery_a);setShow(true);}}
-
-    return (
-      
-        <div style=
-          {{
-            display: 'flex',
-            flexDirection: 'column',
-            margin: 'auto 2%',
-          }}>
-          {achievementsData.length !== 0 ? (
-            <div>        
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Achievement Highlights</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                <Carousel>
-                  {gallery.map((single,index) => (
-                  <Carousel.Item key={"Car"+index}>
-                      <img
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        margin: 'auto 2%',
+      }}
+    >
+      {achievementsData.length !== 0 ? (
+        <div>
+          <Modal
+            show={show}
+            onHide={handleClose}
+            className="achievements-Modal"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title>Achievement Highlights</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <Carousel>
+                {gallery.map((single, index) => (
+                  <Carousel.Item key={'Car' + index}>
+                    <img
                       className="d-block w-100"
                       src={`${process.env.REACT_APP_BASE_URL}/images/${single}`}
                       style={{
-                        display: "block",
-                        maxWidth:"85vw",
-                        maxHeight:"70vh",
-                        width: "auto",
-                        height: "auto"
+                        display: 'block',
+                        maxWidth: '85vw',
+                        maxHeight: '70vh',
+                        width: 'auto',
+                        height: 'auto',
                       }}
-                      alt={"Highlights Pic#" + index}
-                      />
+                      alt={'Highlights Pic#' + index}
+                    />
                   </Carousel.Item>
-                  ))}
-                  </Carousel>
-                </Modal.Body>
-              </Modal>
-            <div className="achievements-limelight">
-                <h1 dangerouslySetInnerHTML={{__html:"HALL OF FAME "+year}}></h1>
-            </div>
-              {achievementsData.map((single,index) => (
-                <Card key={"card"+index} single={single} handleClick={(e)=>{handleShow(e,single.pics_url)}}/>
-              ))}
-            </div>
-          ) : null}
+                ))}
+              </Carousel>
+            </Modal.Body>
+          </Modal>
+          <div className="achievements-limelight">
+            <h1
+              dangerouslySetInnerHTML={{ __html: 'HALL OF FAME ' + year }}
+            ></h1>
+          </div>
+          {achievementsData.map((single, index) => (
+            <Card
+              key={'card' + index}
+              single={single}
+              handleClick={(e) => {
+                handleShow(e, single.pics_url);
+              }}
+            />
+          ))}
         </div>
-      );
-  };
-  
-  export default AchievementsContainer;
+      ) : null}
+    </div>
+  );
+};
+
+export default AchievementsContainer;
--- src/Components/AchievementsPage/yearlyTabs/yearlyTabs.js
+++ src/Components/AchievementsPage/yearlyTabs/yearlyTabs.js
@@ -1,37 +1,37 @@
 import React, { useState, useEffect } from 'react';
-import { Tabs,Tab } from 'react-bootstrap';
+import { Tabs, Tab } from 'react-bootstrap';
 import AchievementsContainer from '../AchievementsContainer/AchievementsContainer';
 import './yearlyTabs.css';
 
-const YearlyTabs = ({
-  allData
-}) => {
-    var currYear;
-    currYear=new Date().getFullYear();
-    const [key, setKey] = useState(`${currYear}`);
-    var array=Object.keys(allData)
-    array.sort()
-    const min = parseInt(array[0])
-    const max = parseInt(array[array.length-1])
-    var years=[]
-    for (var i = max; i >= min; i--) {
-      years.push(i);
-    }
-    var yearlyData=allData[key]
-    return (
-      <Tabs
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-      >
-        {years.map((value, index) => {
-            return (
-              <Tab eventKey={value} key={"year-tab-"+value} title={value} disabled={array.indexOf(`${value}`)==-1}>
-              <AchievementsContainer achievementsData={yearlyData} year={value} />
-              </Tab>
-            )
-        })}
-      </Tabs>
-    );
+const YearlyTabs = ({ allData }) => {
+  var currYear;
+  currYear = new Date().getFullYear();
+  const [key, setKey] = useState(`${currYear}`);
+  var array = Object.keys(allData);
+  array.sort();
+  const min = parseInt(array[0]);
+  const max = parseInt(array[array.length - 1]);
+  var years = [];
+  for (var i = max; i >= min; i--) {
+    years.push(i);
   }
-  
-  export default YearlyTabs;
+  var yearlyData = allData[key];
+  return (
+    <Tabs activeKey={key} onSelect={(k) => setKey(k)}>
+      {years.map((value, index) => {
+        return (
+          <Tab
+            eventKey={value}
+            key={'year-tab-' + value}
+            title={value}
+            disabled={array.indexOf(`${value}`) == -1}
+          >
+            <AchievementsContainer achievementsData={yearlyData} year={value} />
+          </Tab>
+        );
+      })}
+    </Tabs>
+  );
+};
+
+export default YearlyTabs;
