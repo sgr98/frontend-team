722035b7777b2beef42750c70092d53badb0d5cf
NobleMathews <55821103+NobleMathews@users.noreply.github.com>
2020-07-31 10:22:05 +0530

    Min - Level
    next pushing fixes required

--- src/Components/WebTeamPage/ContributorsDynamic/ContributorRanker/ContributorRanker.js
+++ src/Components/WebTeamPage/ContributorsDynamic/ContributorRanker/ContributorRanker.js
@@ -14,7 +14,6 @@ const ContributorRanker = ({target}) => {
     if(data.winners)
     return(
         <div className="Contributors-Top-Weekly">
-            <div className="main">
         <div className="center">
           <div className="top3">
             <a className="two item" href={data.winners[1].url} target="_blank">
@@ -22,10 +21,10 @@ const ContributorRanker = ({target}) => {
               </div>
               <div className="pic" style={{backgroundImage: `url(${data.winners[1].img})`}} />
               <div className="name">
-                {data.winners[1].id};
+                {data.winners[1].id}
               </div>
               <div className="score">
-                {(data.winners[1].weekly.a * Math.ceil((data.winners[1].weekly.c)/data.total_c))};
+                {(data.winners[1].weekly.a * Math.ceil((data.winners[1].weekly.c)/data.total_c))}
               </div>
             </a>
             <a className="one item" href={data.winners[0].url} target="_blank">
@@ -33,10 +32,10 @@ const ContributorRanker = ({target}) => {
               </div>
               <div className="pic" style={{backgroundImage: `url(${data.winners[0].img})`}} />
               <div className="name">
-                {data.winners[0].id};
+                {data.winners[0].id}
               </div>
               <div className="score">
-                {(data.winners[0].weekly.a * Math.ceil((data.winners[0].weekly.c)/data.total_c))};
+                {(data.winners[0].weekly.a * Math.ceil((data.winners[0].weekly.c)/data.total_c))}
               </div>
             </a>
             <a className="three item" href={data.winners[2].url} target="_blank">
@@ -44,10 +43,10 @@ const ContributorRanker = ({target}) => {
               </div>
               <div className="pic" style={{backgroundImage: `url(${data.winners[2].img})`}} />
               <div className="name">
-                {data.winners[2].id};
+                {data.winners[2].id}
               </div>
               <div className="score">
-                {(data.winners[2].weekly.a * Math.ceil((data.winners[2].weekly.c)/data.total_c))};
+                {(data.winners[2].weekly.a * Math.ceil((data.winners[2].weekly.c)/data.total_c))}
               </div>
             </a>
           </div>
@@ -56,26 +55,24 @@ const ContributorRanker = ({target}) => {
             <a href={author.url} target="_blank">          
               <div className="item">
                 <div className="pos">
-                  {index + 1};
+                  {index + 1}
                 </div>
                 <div className="pic" style={{backgroundImage: `url(${author.img})`}} />
                 <div className="name">
-                  {author.id};
+                  {author.id}
                 </div>
                 <div className="score">
-                  {author.total};
+                  {author.total}
                 </div>
               </div>
             </a>
             )}
           </div>
-          <h5 style={{color: 'lightgray'}}>* If your name doesnt appear on the list please ensure your pull requests have been approved. Also ensure you have a public email id linked with your commits within github </h5>
         </div>
-      </div>
         </div>
     )
     else
     return(<div></div>)
-};
+}
 
 export default ContributorRanker;
--- src/Components/WebTeamPage/ContributorsDynamic/ContributorsDynamic.js
+++ src/Components/WebTeamPage/ContributorsDynamic/ContributorsDynamic.js
@@ -5,7 +5,11 @@ import './ContributorsDynamic.css';
 export const ContributorsDynamic = () => {
     return (
       <div>
+        <div className="ContributorDynamic-Container">
         <ContributorRanker target={"backend"} />
+        <ContributorRanker target={"frontend"} />
+        </div>
+        <h5 style={{color: 'lightgray'}}>* If your name doesnt appear on the list please ensure your pull requests have been approved. Also ensure you have a public email id linked with your commits within github </h5>
       </div>
     );
   };
